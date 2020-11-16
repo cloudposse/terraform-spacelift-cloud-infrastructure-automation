ea75041d9bac7b71f9287d1b1c75f363af754ddc
Dan Meyers <danjbh@users.noreply.github.com>
2020-11-16 15:57:43 -0800

    Updating var type on environment module

--- modules/environment/variables.tf
+++ modules/environment/variables.tf
@@ -28,7 +28,7 @@ variable "push_policy_id" {
 }
 
 variable "environment_values" {
-  type        = map
+  type        = any
   default     = {}
   description = "The global values applied to all workspaces within the environment."
 }
