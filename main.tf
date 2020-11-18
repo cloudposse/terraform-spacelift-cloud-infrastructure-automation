b6d1b3758249cd993e82f18d83d0612193bbb19b
Dan Meyers <danjbh@users.noreply.github.com>
2020-11-17 20:35:23 -0800

    Disabling policy attachments for testing

--- main.tf
+++ main.tf
@@ -65,9 +65,9 @@ data "spacelift_current_stack" "this" {
   count = var.external_execution ? 0 : 1
 }
 
-resource "spacelift_policy_attachment" "trigger_env" {
-  count = var.external_execution ? 0 : 1
+# resource "spacelift_policy_attachment" "trigger_env" {
+#   count = var.external_execution ? 0 : 1
 
-  policy_id = spacelift_policy.trigger_env.id
-  stack_id  = data.spacelift_current_stack.this[0].id
-}
+#   policy_id = spacelift_policy.trigger_env.id
+#   stack_id  = data.spacelift_current_stack.this[0].id
+# }
