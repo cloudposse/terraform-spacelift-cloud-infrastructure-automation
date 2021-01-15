f78ad5e0e4be9040da0e4acba59bc03c99318f65
Dan Meyers <danjbh@users.noreply.github.com>
2021-01-15 09:31:45 -0800

    Enabling global trigger policy

--- main.tf
+++ main.tf
@@ -62,15 +62,14 @@ resource "spacelift_policy" "push" {
   body = file("${path.module}/policies/push-stack.rego")
 }
 
-# Disabled for local testing
-# data "spacelift_current_stack" "this" {
-#   count = var.external_execution ? 0 : 1
-# }
+data "spacelift_current_stack" "this" {
+  count = var.external_execution ? 0 : 1
+}
 
-# # Attach the Environment Trigger Policy to the current stack
-# resource "spacelift_policy_attachment" "trigger_global" {
-#   count = var.external_execution ? 0 : 1
+# Attach the Environment Trigger Policy to the current stack
+resource "spacelift_policy_attachment" "trigger_global" {
+  count = var.external_execution ? 0 : 1
 
-#   policy_id = spacelift_policy.trigger_global.id
-#   stack_id  = data.spacelift_current_stack.this[0].id
-# }
+  policy_id = spacelift_policy.trigger_global.id
+  stack_id  = data.spacelift_current_stack.this[0].id
+}
