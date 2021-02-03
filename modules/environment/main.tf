13a77348ad48a349685f69513d4773ffcc13955a
Dan Meyers <danjbh@users.noreply.github.com>
2021-02-03 12:38:10 -0800

    Bumping stack-config module & adding inherited component vars to Spacelift config

--- modules/environment/main.tf
+++ modules/environment/main.tf
@@ -13,11 +13,19 @@ module "stacks" {
   repository        = var.repository
   branch            = coalesce(try(each.value.branch, null), var.branch)
   manage_state      = var.manage_state
-  component_vars    = { for k, v in merge(var.stack_vars, try(each.value.vars, {})) : k => jsonencode(v) }
   terraform_version = coalesce(try(each.value.terraform_version, null), var.terraform_version)
   worker_pool_id    = var.worker_pool_id
   runner_image      = try(var.runner_image, null)
   triggers          = coalesce(try(each.value.triggers, null), [])
   trigger_policy_id = var.trigger_policy_id
   push_policy_id    = var.push_policy_id
+
+  component_vars    = { 
+    for k, v in 
+      merge(
+          var.stack_vars,
+          try(each.value.vars, {}),
+          try(var.components[each.value["component_name"]]["vars"], {})
+      ) : k => jsonencode(v) 
+  }
 }
