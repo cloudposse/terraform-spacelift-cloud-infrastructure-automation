8441c321dc79e08f30e50362a1bfe10561989716
Dan Meyers <danjbh@users.noreply.github.com>
2021-02-08 19:14:56 -0800

    Removing some old stuff

--- modules/stack/main.tf
+++ modules/stack/main.tf
@@ -23,9 +23,6 @@ resource "spacelift_mounted_file" "stack_config" {
   stack_id      = spacelift_stack.default[0].id
   relative_path = format("source/%s/spacelift.auto.tfvars.json", var.component_root)
   content = base64encode(var.component_vars)
-  # content = base64encode(jsonencode({
-  #   for k, v in var.component_vars : k => jsondecode(v)
-  # }))
 
   write_only = false
 }
