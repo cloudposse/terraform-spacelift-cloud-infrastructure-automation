03859df878c613290cded8516323697866e5aafb
aknysh <andriy.knysh@gmail.com>
2021-04-26 15:19:11 -0400

    Disable automatic retries policy

--- main.tf
+++ main.tf
@@ -58,12 +58,12 @@ resource "spacelift_policy" "trigger_dependency" {
 }
 
 # Define the automatic retries trigger policy that allows automatically restarting the failed run
-resource "spacelift_policy" "trigger_retries" {
-  type = "TRIGGER"
-
-  name = "Failed Run Automatic Retries Trigger Policy"
-  body = file("${path.module}/policies/trigger-retries.rego")
-}
+# resource "spacelift_policy" "trigger_retries" {
+#   type = "TRIGGER"
+#
+#  name = "Failed Run Automatic Retries Trigger Policy"
+#  body = file("${path.module}/policies/trigger-retries.rego")
+#}
 
 # Define the global "git push" policy that causes executions on stacks when `<component_root>/*.tf` is modified
 resource "spacelift_policy" "push" {
