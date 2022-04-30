3dbb63f925749cb88d9da823b4a2f50ac5a3eafd
nitrocode <nitrocode@users.noreply.github.com>
2022-04-30 02:12:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "spacelift_policy" "default" {
   # name = GIT_PUSH Proposed Run Policy
   # body = file("./catalog/policies/git_push.proposed-run.rego")
   type = try(each.type, upper(split(".", each.key)[0]))
-  name = try(each.name", format("%s %s Policy", upper(split(".", each.name)[0]), title(replace(split(".", each.key)[1], "-", " "))))
+  name = try(each.name, format("%s %s Policy", upper(split(".", each.name)[0]), title(replace(split(".", each.key)[1], "-", " "))))
   body = file(format("%s/%s/%s.rego", path.module, var.policies_path, each.key))
 
   labels = try(each.labels, [])
