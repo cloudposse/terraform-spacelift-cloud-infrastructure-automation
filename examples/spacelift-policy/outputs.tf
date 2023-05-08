109bb08ab5da1d469ac29c80b9509a77426082b6
Matt Calhoun <matt@mattcalhoun.com>
2023-05-08 15:32:58 -0400

    add descriptions

--- examples/spacelift-policy/outputs.tf
+++ examples/spacelift-policy/outputs.tf
@@ -1,7 +1,9 @@
 output "inline_policy" {
-  value = module.inline_policy.policy
+  value       = module.inline_policy.policy
+  description = "The inline policy that was created"
 }
 
 output "catalog_policy" {
-  value = module.catalog_policy.policy
+  value       = module.catalog_policy.policy
+  description = "The catalog policy that was created"
 }
