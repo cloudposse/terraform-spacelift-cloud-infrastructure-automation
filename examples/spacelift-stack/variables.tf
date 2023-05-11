952c3fadf38980f5415a335131d4cda6ab18c756
Matt Calhoun <matt@mattcalhoun.com>
2023-05-11 13:45:58 -0400

    sort variables

--- examples/spacelift-stack/variables.tf
+++ examples/spacelift-stack/variables.tf
@@ -1,12 +1,18 @@
-variable "local_preview_enabled" {
+variable "administrative" {
   type        = bool
-  description = "Indicates whether local preview runs can be triggered on this Stack"
+  description = "Whether this stack can manage other stacks"
   default     = false
 }
 
-variable "repository" {
+variable "atmos_stack_name" {
   type        = string
-  description = "The name of your infrastructure repo"
+  description = "The name of the atmos stack"
+}
+
+variable "autodeploy" {
+  type        = bool
+  description = "Controls the Spacelift 'autodeploy' option for a stack"
+  default     = false
 }
 
 variable "branch" {
@@ -15,30 +21,20 @@ variable "branch" {
   default     = "main"
 }
 
-variable "terraform_version" {
-  type        = string
-  description = "Specify the version of Terraform to use for the stack"
-  default     = null
-}
-
-variable "component_root" {
-  type        = string
-  description = "The path, relative to the root of the repository, where the component can be found"
-}
-
-variable "atmos_stack_name" {
-  type        = string
-  description = "The name of the atmos stack"
-}
-
-variable "stack_name" {
-  type        = string
-  description = "The name of the Spacelift stack"
+variable "component_env" {
+  type        = any
+  default     = {}
+  description = "Map of component ENV variables"
 }
 
 variable "component_name" {
   type        = string
   description = "The name of the concrete component (typically a directory name)"
+}
+
+variable "component_root" {
+  type        = string
+  description = "The path, relative to the root of the repository, where the component can be found"
 }
 
 variable "component_vars" {
@@ -47,15 +43,21 @@ variable "component_vars" {
   description = "All Terraform values to be applied to the stack via a mounted file"
 }
 
-variable "component_env" {
-  type        = any
-  default     = {}
-  description = "Map of component ENV variables"
+variable "description" {
+  type        = string
+  description = "Specify description of stack"
+  default     = null
 }
 
-variable "autodeploy" {
+variable "labels" {
+  type        = list(string)
+  description = "A list of labels for the stack"
+  default     = []
+}
+
+variable "local_preview_enabled" {
   type        = bool
-  description = "Controls the Spacelift 'autodeploy' option for a stack"
+  description = "Indicates whether local preview runs can be triggered on this Stack"
   default     = false
 }
 
@@ -65,29 +67,9 @@ variable "manage_state" {
   default     = true
 }
 
-variable "labels" {
-  type        = list(string)
-  description = "A list of labels for the stack"
-  default     = []
-}
-
-
-variable "stack_destructor_enabled" {
-  type        = bool
-  description = "Flag to enable/disable the stack destructor to destroy the resources of the stack before deleting the stack itself"
-  default     = false
-}
-
-variable "administrative" {
-  type        = bool
-  description = "Whether this stack can manage other stacks"
-  default     = false
-}
-
-variable "description" {
+variable "repository" {
   type        = string
-  description = "Specify description of stack"
-  default     = null
+  description = "The name of your infrastructure repo"
 }
 
 variable "space_id" {
@@ -96,3 +78,19 @@ variable "space_id" {
   default     = "root"
 }
 
+variable "stack_destructor_enabled" {
+  type        = bool
+  description = "Flag to enable/disable the stack destructor to destroy the resources of the stack before deleting the stack itself"
+  default     = false
+}
+
+variable "stack_name" {
+  type        = string
+  description = "The name of the Spacelift stack"
+}
+
+variable "terraform_version" {
+  type        = string
+  description = "Specify the version of Terraform to use for the stack"
+  default     = null
+}
