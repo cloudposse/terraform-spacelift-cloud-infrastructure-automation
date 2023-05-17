9e5a127103e7a2282349d3baf291981a1e40dc21
Matt Calhoun <matt@mattcalhoun.com>
2023-05-17 11:26:56 -0400

    add filter for root admin

--- modules/spacelift-stacks-from-atmos-config/main.tf
+++ modules/spacelift-stacks-from-atmos-config/main.tf
@@ -27,6 +27,11 @@ locals {
       (var.context_filters.administrative == true && lookup(v.settings.spacelift, "administrative", false) == true)      # if set to true return only administrative stacks
     ) &&
     (
+      (try(var.context_filters.root_administrative, null) == null) ||                                                              # If not set return all stacks
+      (var.context_filters.root_administrative == false && lookup(v.settings.spacelift, "root_administrative", false) == false) || # if set to false return only non-root admin stacks
+      (var.context_filters.root_administrative == true && lookup(v.settings.spacelift, "root_administrative", false) == true)      # if set to true return only non-root admin stacks
+    ) &&
+    (
       length(var.context_filters.tags) == 0 || (
         lookup(v.vars, "tags", null) != null &&
         contains([
