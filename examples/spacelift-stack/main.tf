07157c66e0c7f2b52dbcb656aaae76936325956b
Matt Calhoun <matt@mattcalhoun.com>
2023-06-08 12:43:07 -0400

    update example module

--- examples/spacelift-stack/main.tf
+++ examples/spacelift-stack/main.tf
@@ -3,19 +3,22 @@ provider "spacelift" {}
 module "stack" {
   source = "../../modules/spacelift-stack"
 
-  stack_name               = var.stack_name
-  component_name           = var.component_name
-  atmos_stack_name         = var.atmos_stack_name
-  description              = var.description
   administrative           = var.administrative
+  atmos_stack_name         = var.atmos_stack_name
   autodeploy               = var.autodeploy
-  repository               = var.repository
   branch                   = var.branch
+  component_env            = var.component_env
+  component_name           = var.component_name
   component_root           = var.component_root
-  manage_state             = var.manage_state
+  component_vars           = var.component_vars
+  description              = var.description
   labels                   = var.labels
   local_preview_enabled    = var.local_preview_enabled
+  manage_state             = var.manage_state
+  repository               = var.repository
+  space_id                 = var.space_id
   stack_destructor_enabled = var.stack_destructor_enabled
+  stack_name               = var.stack_name
   terraform_version        = var.terraform_version
 }
 
